f81414cdac79ba2178670432969431f582b436db
fryl0ch <7390162+fryl0ch@users.noreply.github.com>
2024-12-19 23:29:29 -0500

    that's no array, its a collection!

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ client.on("messageCreate", message => {
   if(message.content.startsWith("!")){
     console.log(message.author.username, `[${message.author.global_name}]:`, message.content);
     
-    if (client.commands.includes(message.content.split()[0].replace('!','')))
+    if (client.commands.find(command => command.name === message.content.split()[0].replace('!','')))
       message.channel.send(`command '${message.content}' recieved from ${message.author.username}`);
     else
       message.channel.send(`command '${message.content}' not found`);
