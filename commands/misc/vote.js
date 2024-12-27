884318b0393cacfd4a82d4af365b8a166edd18ab
fryl0ch <7390162+fryl0ch@users.noreply.github.com>
2024-12-27 00:07:11 -0500

    start making this a class, add back the update time left every 10 seconds

--- commands/misc/vote.js
+++ commands/misc/vote.js
@@ -4,21 +4,60 @@ const now = function() {
   return (new Date()).valueOf();
 }
 
+class Poll {
+  startTime = now();
+  vote_results = {};
 
-let startTime;
-let vote_results = {};
+  options = [];
+  options_kv = {};
 
-let options = [];
-let options_kv = {};
+  updateInterval;
 
-let vote_reason;
-let vote_duration = 30; // in seconds
+  vote_duration = 30; // in seconds
 
-export function getVoteCountFor(value)
-{
-  if (vote_results[value])
-    return vote_results[value].length;
-  else return 0;
+  getVoteCountFor(value)
+  {
+    if (this.vote_results[value])
+      return this.vote_results[value].length;
+    else return 0;
+  }
+
+  clearUpdateInterval() {
+      clearInterval(this.updateInterval);
+  }
+
+  async listenForVotes(voteMessage, duration_ms) {
+    try {
+      console.log('vote started ', 'duration_ms', duration_ms)
+      let voteWatcher = await voteMessage.awaitMessageComponent({time: (this.vote_duration*1_000) - (now() - this.startTime) });
+      voteWatcher.deferUpdate();
+      // only one vote per voter
+      for (let option of Object.keys(this.vote_results))
+        if (this.vote_results[option].includes(voteWatcher.member.displayName))
+          this.vote_results[option] = this.vote_results[option].filter(o => o !== voteWatcher.member.displayName);
+
+      if (! this.vote_results[voteWatcher.customId] ) this.vote_results[voteWatcher.customId] = [];
+      this.vote_results[voteWatcher.customId].push(voteWatcher.member.displayName);
+      await updateVoteCounts(this, voteMessage,false);
+
+      let elapsed_time = now() - this.startTime;
+      if (elapsed_time < duration_ms)
+      {
+        let time_left = ((this.vote_duration * 1_000) - elapsed_time);
+        console.log('vote recieved, listing for ' + time_left + " more ms");
+        await this.listenForVotes(voteMessage, time_left);
+      }
+    } catch (error) {
+      if (error.code === 'InteractionCollectorError') // out of time
+      {
+        console.log('voting finished');
+        await updateVoteCounts(this, voteMessage, true);
+        voteMessage.reply('Voting has ended.');
+      }
+      else
+        throw error;
+    }  
+  }
 }
 
 export const data = new SlashCommandBuilder()
@@ -56,60 +95,56 @@ export const data = new SlashCommandBuilder()
         .setRequired(false));
 
 export const execute = async function (interaction) {
-  startTime = now();
-  vote_results = [];
+  let the_poll = new Poll();
 
-  vote_reason = interaction.options.getString('question');
-
-  options = [];
-  options_kv = {};
+  the_poll.vote_reason = interaction.options.getString('question');
 
   if (interaction.options.getString('option1')) 
   {
-    options.push(interaction.options.getString('option1'));
-    options_kv.vote_1 = interaction.options.getString('option1');
+    the_poll.push(interaction.options.getString('option1'));
+    the_poll.vote_1 = interaction.options.getString('option1');
   }
   if (interaction.options.getString('option2'))
   {
-    options.push(interaction.options.getString('option2'));
-    options_kv.vote_2 = interaction.options.getString('option2');
+    the_poll.options.push(interaction.options.getString('option2'));
+    the_poll.options_kv.vote_2 = interaction.options.getString('option2');
   }
   if (interaction.options.getString('option3'))
   {
-    options.push(interaction.options.getString('option3'));
-    options_kv.vote_3 = interaction.options.getString('option3');
+    the_poll.options.push(interaction.options.getString('option3'));
+    the_poll.options_kv.vote_3 = interaction.options.getString('option3');
   }
   if (interaction.options.getString('option4'))
   {
-    options.push(interaction.options.getString('option4'));
-    options_kv.vote_4 = interaction.options.getString('option4');
+    the_poll.options.push(interaction.options.getString('option4'));
+    the_poll.options_kv.vote_4 = interaction.options.getString('option4');
   }
   if (interaction.options.getString('option5'))
   {
-    options.push(interaction.options.getString('option5'));
-    options_kv.vote_5 = interaction.options.getString('option5');
+    the_poll.options.push(interaction.options.getString('option5'));
+    the_poll.options_kv.vote_5 = interaction.options.getString('option5');
   }
 
   if (interaction.options.getInteger('duration'))
-    vote_duration = Number.parseInt(interaction.options.getInteger('duration'));
+    the_poll.vote_duration = Number.parseInt(interaction.options.getInteger('duration'));
+
 
   const voteEmbed = new EmbedBuilder()
     .setColor(0x0099FF)
-    //.setTitle(vote_reason)
-    .setTitle(vote_reason);
+    .setTitle(the_poll.vote_reason);
 
   let counter = 1;
   let vote_row = new ActionRowBuilder();
 
-  if (options.filter(o => o).length > 0)
+  if (the_poll.options.filter(o => o).length > 0)
   {
-    for (let option of options.filter(o => o))
+    for (let option of the_poll.options.filter(o => o))
     {
       voteEmbed.addFields({name: " ", value: option});
 
       const vote_button = new ButtonBuilder()
         .setCustomId(`vote_${counter}`)
-        .setLabel(option + " (" + getVoteCountFor(option) + ")")
+        .setLabel(option + " (" + this.getVoteCountFor(option) + ")")
         .setStyle(ButtonStyle.Secondary);
       vote_row.addComponents(vote_button);
       counter++;
@@ -120,8 +155,8 @@ export const execute = async function (interaction) {
     const yes_vote_value = "Yes ✅";
     const no_vote_value = "No ❌";
 
-    options = [yes_vote_value, no_vote_value];
-    options_kv = {vote_1: yes_vote_value, vote_2: no_vote_value};
+    the_poll.options = [yes_vote_value, no_vote_value];
+    the_poll.options_kv = {vote_1: yes_vote_value, vote_2: no_vote_value};
     const yes_vote_button = new ButtonBuilder()
       .setCustomId(`vote_1`)
       .setLabel(yes_vote_value)
@@ -139,97 +174,67 @@ export const execute = async function (interaction) {
     components: [vote_row]
   });
 
-  for (let option of Object.keys(options_kv))
+  for (let option of Object.keys(the_poll.options_kv))
   {
-    vote_results[option] = [];
+    the_poll.vote_results[option] = [];
   }
 
-  await listenForVotes(voteMessage, vote_duration*1_000);
+  the_poll.updateInterval = setInterval(updateVoteCounts, 5_000, the_poll, voteMessage);
+  setTimeout(clearInterval, (the_poll.vote_duration*1000) - 4_000, the_poll.updateInterval);
+
+  await the_poll.listenForVotes(voteMessage, the_poll.vote_duration*1_000);
 }
 
-const updateVoteCounts = async function(voteMessage, done=false)
-{
-  let resultsEmbed = new EmbedBuilder()
-    .setColor(0x0099FF)
-    .setTitle(vote_reason);
-
-  if (!done)
-    resultsEmbed.addFields({name: " ", value: `⏳ time remaining: ${Math.round(( (vote_duration*1_000) - (now() - startTime) )/1000)} seconds`});
-  else
-    resultsEmbed.addFields({name: " ", value: `voting has ended.`});
-
-  if (Object.keys(vote_results).length)
+async function updateVoteCounts(poll, voteMessage, done=false)
   {
-    for (let result of Object.keys(vote_results))
+    let resultsEmbed = new EmbedBuilder()
+      .setColor(0x0099FF)
+      .setTitle(poll.vote_reason);
+
+    if (!done)
+      resultsEmbed.addFields({name: " ", value: `⏳ time remaining: ${Math.round(( (poll.vote_duration*1_000) - (now() - poll.startTime) )/1000)} seconds`});
+    else
+      resultsEmbed.addFields({name: " ", value: `voting has ended.`});
+
+    if (Object.keys(poll.vote_results).length)
     {
-      resultsEmbed.addFields({name: " ", value: `${options_kv[result]} recieved ${getVoteCountFor(result)} vote${getVoteCountFor(result) > 1 ? 's' : ''}`});
-    }
-  }
-  else
-    resultsEmbed.addFields({name: " ", value: `no votes were cast`});
-
-  let vote_row = new ActionRowBuilder();
-
-  if (options.filter(o => o).length > 0)
-  {
-    let counter = 1;
-    for (let option of options)
-    {
-      //console.log('id:', counter, 'opt:', option);
-      const vote_button = new ButtonBuilder()
-        .setCustomId(`vote_${counter}`)
-        .setLabel(option)
-        .setStyle(ButtonStyle.Secondary);
-      vote_row.addComponents(vote_button);
-      counter++;
-    }
-  }
-
-  if (!done)
-  {
-    return await voteMessage.edit({
-      embeds: [resultsEmbed],
-      components: [vote_row] 
-    });
-  }
-  else
-  {
-    return await voteMessage.edit({
-      embeds: [resultsEmbed],
-      components: [] // remove vote buttons
-    });
-  }
-}
-
-const listenForVotes = async function(voteMessage, duration_ms) {
-  try {
-    console.log('vote started ', 'duration_ms', duration_ms)
-    let voteWatcher = await voteMessage.awaitMessageComponent({time: (vote_duration*1_000) - (now() - startTime) });
-    voteWatcher.deferUpdate();
-    // only one vote per voter
-    for (let option of Object.keys(vote_results))
-      if (vote_results[option].includes(voteWatcher.member.displayName))
-        vote_results[option] = vote_results[option].filter(o => o !== voteWatcher.member.displayName);
-
-    if (! vote_results[voteWatcher.customId] ) vote_results[voteWatcher.customId] = [];
-    vote_results[voteWatcher.customId].push(voteWatcher.member.displayName);
-    await updateVoteCounts(voteMessage,false);
-
-    let elapsed_time = now() - startTime;
-    if (elapsed_time < duration_ms)
-    {
-      let time_left = ((vote_duration * 1_000) - elapsed_time);
-      console.log('vote recieved, listing for ' + time_left + " more ms");
-      await listenForVotes(voteMessage, time_left);
-    }
-  } catch (error) {
-    if (error.code === 'InteractionCollectorError') // out of time
-    {
-      console.log('voting finished', error);
-      await updateVoteCounts(voteMessage, true);
-      await voteMessage.reply('Voting has ended.');
+      for (let result of Object.keys(poll.vote_results))
+      {
+        resultsEmbed.addFields({name: " ", value: `${poll.options_kv[result]} recieved ${poll.getVoteCountFor(result)} vote${poll.getVoteCountFor(result) > 1 ? 's' : ''}`});
+      }
     }
     else
-      throw error;
+      resultsEmbed.addFields({name: " ", value: `no votes were cast`});
+
+    let vote_row = new ActionRowBuilder();
+
+    if (poll.options.filter(o => o).length > 0)
+    {
+      let counter = 1;
+      for (let option of poll.options)
+      {
+        //console.log('id:', counter, 'opt:', option);
+        const vote_button = new ButtonBuilder()
+          .setCustomId(`vote_${counter}`)
+          .setLabel(option)
+          .setStyle(ButtonStyle.Secondary);
+        vote_row.addComponents(vote_button);
+        counter++;
+      }
+    }
+
+    if (!done)
+    {
+      return await voteMessage.edit({
+        embeds: [resultsEmbed],
+        components: [vote_row] 
+      });
+    }
+    else
+    {
+      return await voteMessage.edit({
+        embeds: [resultsEmbed],
+        components: [] // remove vote buttons
+      });
+    }
   }
-}
