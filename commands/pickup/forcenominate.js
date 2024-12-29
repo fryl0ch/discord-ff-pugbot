be410ec245f966c690113577fa26b1b76c936665
fryl0ch <7390162+fryl0ch@users.noreply.github.com>
2024-12-28 21:53:06 -0500

    !forcenominate

---
+++ commands/pickup/forcenominate.js
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+import { pickup } from '../../pickup-game.js'
+
+export const data = new SlashCommandBuilder()
+		.setName('forecenominate')
+		.setDescription('force !nominate a map for a user')
+		.addStringOption(option =>
+	      option.setName('map')
+	        .setDescription('The name of the map you want to nominate')
+	        .setMaxLength(420)
+	        .setRequired(true))
+		.addUserOption(option => option.setName('target').setDescription('The user to !nominate as').setRequired(true));
+
+export const execute = async function (interaction, options=null) {
+	let map;
+	const target = interaction.options.getUser('target');
+
+	if (options)
+	{
+		map = options.map;
+	}
+	else
+	{
+		map = interaction.options.getString('map');
+	}
+
+	let nominator = target.displayName;
+	return await interaction.reply(pickup.nominate(map,nominator));
+}
